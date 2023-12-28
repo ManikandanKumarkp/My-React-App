08a658666db4437c370954f3170f248bdbf04707
Manikandan <manikandankumarkp@gmail.com>
2023-12-28 15:16:47 +0530

    some edited

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         hello world
         </p>
         <a
           className="App-link"
@@ -15,7 +16,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
